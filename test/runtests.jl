da5b05f94d74e695b1ead357f68da1abd8f22dff
Jacob Quinn <quinn.jacobd@gmail.com>
2021-10-29 12:28:45 -0600

    add initial test (#2)
    * add initial test
    
    * fix test
    
    * fix ci

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,31 @@
 using Test, Interfaces
 
+abstract type Iterable end
+
+@interface Iterable begin
+
+    iterate(::Iterable)
+    iterate(::Iterable, st)
+
+    Base.IteratorSize(::Type{Iterable})::Union{Base.HasLength, Base.HasShape, Base.IsInfinite, Base.SizeUnknown}
+
+    if Base.IteratorSize(Iterable) == Base.HasLength()
+        length(::Iterable)
+    elseif Base.IteratorSize(Iterable) isa Base.HasShape
+        length(::Iterable)
+        size(::Iterable)
+    end
+
+    Base.IteratorEltype(::Type{Iterable})::Union{Base.HasEltype, Base.EltypeUnknown}
+
+    if Base.IteratorEltype(Iterable) == Base.HasEltype()
+        eltype(::Iterable)
+    end
+
+end
+
+@test Interfaces.implements(Int, Iterable; mods=[Base])
+
+struct Foo end
+
+@test_throws InterfaceImplementationError Interfaces.implements(Foo, Iterable)
