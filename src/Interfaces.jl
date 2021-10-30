ddd487ef78832380672008a3dc6d1abad5ffb3f9
Jacob Quinn <quinn.jacobd@gmail.com>
2021-10-30 01:21:12 -0600

    More work to support Tables.jl interface definitions (#3)
    * More work to support Tables.jl interface definitions
    
    * fix test

--- src/Interfaces.jl
+++ src/Interfaces.jl
@@ -7,11 +7,12 @@ struct InterfaceImplementationError <: Exception
 end
 
 isinterfacetype(::Type{T}) where {T} = false
+isinterfacetype(::Type{Type{T}}) where {T} = isinterfacetype(T)
 
 function interface end
 
 struct Interface
-    name::Symbol
+    name
     exprs::Vector{Any}
 end
 
@@ -19,15 +20,18 @@ end
 
 function implements end
 
+implements(::Type{Type{T}}, ::Type{Type{S}}, mods::Vector{Module}=[parentmodule(T)]) where {T, S} =
+    implements(T, S, mods)
+
 function implemented(f, args, mods)
     impls = Base.methods(f, args, mods)
     return length(impls) > 0
 end
 
-function recursiveswapT!(T::Symbol, expr)
+function recursiveswapT!(T, expr)
     for i = 1:length(expr.args)
         arg = expr.args[i]
-        if arg === T
+        if arg == T
             expr.args[i] = :T
         elseif arg isa Expr
             recursiveswapT!(T, arg)
@@ -68,23 +72,31 @@ requiredmethod(T, nm, args, shouldthrow) = :(Interfaces.implemented($nm, $args, 
 
 @noinline missingmethod(T, f, args, mods) = throw(InterfaceImplementationError("missing `$T` interface method definition: `$(Expr(:call, f, unconvertargs(args)...))`, in module(s): `$mods`"))
 @noinline invalidreturntype(T, f, args, RT1, RT2) = throw(InterfaceImplementationError("invalid return type for `$T` interface method definition: `$(Expr(:call, f, unconvertargs(args)...))`; inferred $RT1, required $RT2"))
+@noinline subtypingrequired(IT, T) = throw(InterfaceImplementationError("interface `$IT` requires implementing types to subtype, like: `struct $T <: $IT`"))
 @noinline atleastonerequired(T, expr) = throw(InterfaceImplementationError("for `$T` interface, one of the following method definitions is required: `$expr`"))
 
-function toimplements!(T::Symbol, arg::Expr, shouldthrow::Bool=true)
+function toimplements!(T, arg::Expr, shouldthrow::Bool=true)
     if arg.head == :call
+        # required method definition
         nm, args = methodparts(T, arg)
         return requiredmethod(T, nm, args, shouldthrow)
     elseif arg.head == :(::)
+        # required method definition and required return type
         nm, args = methodparts(T, arg.args[1])
-        RT = arg.args[2]
+        __RT__ = arg.args[2]
+        sym = gensym()
         return quote
             check = $(requiredmethod(T, nm, args, shouldthrow))
-            RT = Base._return_type($nm, $args)
-            check |= Interfaces.isinterfacetype($RT) ?
-                Interfaces.implements(RT, $RT) : RT <: $RT
-            check || ($shouldthrow && Interfaces.invalidreturntype($nm, $args, RT, $RT))
+            $sym = Interfaces.returntype($nm, $args)
+            # @show $sym, $nm, $args, Interfaces.isinterfacetype($__RT__)
+            check |= Interfaces.isinterfacetype($__RT__) ?
+                Interfaces.implements($sym, $__RT__) : $sym <: $__RT__
+            check || ($shouldthrow && Interfaces.invalidreturntype($nm, $args, $sym, $__RT__))
         end
+    elseif arg.head == :<:
+        return :((T <: $T) || Interfaces.subtypingrequired($T, T))
     elseif arg.head == :if
+        # conditional requirement
         origarg = arg
         recursiveswapT!(T, arg.args[1])
         arg.args[2] = toimplements!(T, arg.args[2])
@@ -101,16 +113,26 @@ function toimplements!(T::Symbol, arg::Expr, shouldthrow::Bool=true)
         end
         return origarg
     elseif arg.head == :||
+        # one of many required
         argcopy = copy(arg)
         origarg = arg
-        while arg.head == :||
+        while true
             arg.args[1] = toimplements!(T, arg.args[1], false)
+            arg.args[2].head == :|| || break
             arg = arg.args[2]
         end
         arg.args[2] = toimplements!(T, arg.args[2], false)
-        return :(($origarg) || Interfaces.atleastonerequired($T, $argcopy))
+        return :(($origarg) || Interfaces.atleastonerequired($T, $(Meta.quot(argcopy))))
     elseif arg.head == :block
+        # not supported at top-level of @interface block
+        # but can be block of if-else or || expressions
         map!(x -> toimplements!(T, x, shouldthrow), arg.args, arg.args)
+        return arg
+    elseif arg.head == :(=)
+        arg.args[1] == :T && error("invalid assignment variable name in @interface definition; must use name other than `T`")
+        expr = arg.args[2]
+        expr.head == :macrocall || error("invalid assignment in @interface definition, may only assign result of `RT = Interfaces.@returntype expr`")
+        recursiveswapT!(T, arg)
         return arg
     else
         throw(ArgumentError("unsupported expression in @interface block for `$T`: `$arg`"))
@@ -118,7 +140,7 @@ function toimplements!(T::Symbol, arg::Expr, shouldthrow::Bool=true)
 end
 
 macro interface(T, block)
-    @assert T isa Symbol
+    @assert T isa Symbol || T.head == :.
     @assert block isa Expr && block.head == :block
     Base.remove_linenums!(block)
     iface = Interface(T, deepcopy(block.args))
@@ -127,7 +149,7 @@ macro interface(T, block)
     return esc(quote
         Interfaces.isinterfacetype(::Type{$T}) = true
         Interfaces.interface(::Type{$T}) = $iface
-        function Interfaces.implements(::Type{T}, ::Type{$T}; mods::Vector{Module}=[parentmodule(T)]) where {T}
+        function Interfaces.implements(::Type{T}, ::Type{$T}, mods::Vector{Module}=[parentmodule(T)]) where {T}
             $block
         end
     end)
@@ -138,6 +160,14 @@ macro implements(T, IT)
         @assert Interfaces.implements($T, $IT)
         Interfaces.implements(::Type{$T}, ::Type{$IT}) = true
     end)
+end
+
+@noinline returntype(@nospecialize(f), @nospecialize(args)) = Base.return_types(f, args)[1]
+
+macro returntype(expr)
+    @assert expr.head == :call
+    nm, args = methodparts(:T, expr)
+    return esc(:(Interfaces.returntype($nm, $args)))
 end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
-using Test, Interfaces
+using Test, Interfaces, Tables
 
 abstract type Iterable end
 
@@ -19,13 +19,80 @@ abstract type Iterable end
     Base.IteratorEltype(::Type{Iterable})::Union{Base.HasEltype, Base.EltypeUnknown}
 
     if Base.IteratorEltype(Iterable) == Base.HasEltype()
-        eltype(::Iterable)
+        eltype(::Iterable) || eltype(::Type{Iterable})
     end
 
 end
 
-@test Interfaces.implements(Int, Iterable; mods=[Base])
+@test Interfaces.implements(Int, Iterable, [Base])
+
+abstract type Indexable end
+
+@interface Indexable begin
+    getindex(::Indexable, i)
+    setindex!(::Indexable, v, i)
+    firstindex(::Indexable)
+    lastindex(::Indexable)
+end
+
+@test Interfaces.implements(Array, Indexable, [Base])
+
+@interface AbstractArray begin
+
+    <: AbstractArray
+    size(::AbstractArray)
+    Base.IndexStyle(::Type{AbstractArray})::Union{Base.IndexLinear, Base.IndexCartesian}
+
+    if Base.IndexStyle(AbstractArray) == Base.IndexLinear()
+        getindex(::AbstractArray, i::Int)
+        setindex!(::AbstractArray, v, i::Int)
+    elseif Base.IndexStyle(AbstractArray) == Base.IndexCartesian()
+        getindex(::AbstractArray, I::Vararg{Int})
+        setindex!(::AbstractArray, v, I::Vararg{Int})
+    end
+
+end
+
+@test Interfaces.implements(Array, AbstractArray, [Base])
 
 struct Foo end
 
+@test_throws InterfaceImplementationError Interfaces.implements(Foo, AbstractArray)
+
 @test_throws InterfaceImplementationError Interfaces.implements(Foo, Iterable)
+
+@interface Tables.AbstractColumns begin
+    Tables.getcolumn(::Tables.AbstractColumns, ::Int)
+    Tables.getcolumn(::Tables.AbstractColumns, ::Symbol)
+    Tables.columnnames(::Tables.AbstractColumns)
+end
+
+@interface Tables.AbstractRow begin
+    Tables.getcolumn(::Tables.AbstractRow, ::Int)
+    Tables.getcolumn(::Tables.AbstractRow, ::Symbol)
+    Tables.columnnames(::Tables.AbstractRow)
+end
+
+abstract type AbstractTable end
+
+@interface AbstractTable begin
+    Tables.isrowtable(::Type{AbstractTable}) ||
+        Tables.rowaccess(::Type{AbstractTable}) ||
+            Tables.columnaccess(::Type{AbstractTable})
+    if Tables.isrowtable(AbstractTable) || Tables.rowaccess(AbstractTable)
+        Tables.rows(::AbstractTable)::Iterable
+        RT = Interfaces.@returntype Tables.rows(::AbstractTable)
+        if Base.IteratorEltype(RT) == Base.HasEltype()
+            eltype(::RT)::Type{Tables.AbstractRow} ||
+                eltype(::Type{RT})::Type{Tables.AbstractRow}
+                
+        end
+    elseif Tables.columnaccess(AbstractTable)
+        Tables.istable(::Type{AbstractTable})
+        Tables.columns(::AbstractTable)::AbstractColumns
+    end
+
+    # @optional schema(::AbstractTable)::Union{Nothing, Tables.Schema}
+end
+
+@test Interfaces.implements(Tables.DictRowTable, AbstractTable)
